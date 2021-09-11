91183021c689dd48aeae8198e9ff07c7fd457ebf
monkeyidesign <sam@monkeyidesign.com>
2021-09-11 18:06:27 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# sample_app
+
+## Getting Started GetX
+```
+https://www.youtube.com/watch?v=ou4iTI3YP_A&list=PLCaS22Sjc8YR32XmudgmVqs49t-eKKr9t&index=2
+```
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  var count = 0.obs;
+
+  @override
+  Widget build(context) => Scaffold(
+      appBar: AppBar(title: Text("counter")),
+      body: Center(
+        child: Obx(() => Text("$count")),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => count++,
+      ));
+}
